0614d41e4cfb9bfff90d188e654a2a3e9c092acf
Agung <agungmasda29@gmail.com>
2021-06-22 01:23:46 +0700

    Initialize template library :lipstick:

---
+++ application/libraries/Template.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Template
+{
+	protected $ci;
+
+	protected $module;
+
+	public function __construct($config)
+	{
+        $this->ci =& get_instance();
+        if (isset($config['module'])) {
+        	$this->module = $config['module'];
+        }
+	}
+
+	public function load($page, $params = array())
+	{
+		$data['page'] = $this->ci->load->view($this->module.'/'.$page, $params, TRUE);
+		$this->ci->load->view($this->module.'/base', array_merge($data, $params), FALSE);
+	}
+}
+
+/* End of file Template.php */
+/* Location: ./application/libraries/Template.php */
