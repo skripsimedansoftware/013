779cfc8800fff0c1715cb0029d9de05be85112af
Agung <agungmasda29@gmail.com>
2021-06-24 00:30:22 +0700

    changed view mode

--- application/views/admin/base.php
+++ application/views/admin/base.php
@@ -266,16 +266,7 @@ desired effect
 
 	<!-- Content Wrapper. Contains page content -->
 	<div class="content-wrapper">
-		<!-- Content Header (Page header) -->
-		<section class="content-header">
-			<h1>Page Header<small>Optional description</small></h1>
-		</section>
-
-		<!-- Main content -->
-		<section class="content container-fluid">
-			<?php echo $page ?>
-		</section>
-		<!-- /.content -->
+		<?php echo $page ?>
 	</div>
 	<!-- /.content-wrapper -->
 
--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -1,3 +1,11 @@
-<div class="row">
-	<div class="col-lg-12">Page Content Here</div>
-</div>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+	<h1>Page Header<small>Optional description</small></h1>
+</section>
+
+<!-- Main content -->
+<section class="content container-fluid">
+	<div class="row">
+		<div class="col-lg-12">Page Content Here</div>
+	</div>
+</section>
