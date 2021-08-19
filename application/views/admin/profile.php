1a9e5d7182d8aa8abc27103cbc68138202018ea0
Agung Dirgantara <agungmasda29@gmail.com>
2021-08-20 06:45:43 +0700

    remove page

--- application/views/admin/profile.php
+++
@@ -1,8 +0,0 @@
-<!-- Content Header (Page header) -->
-<section class="content-header">
-	<h1>Administrator<small>Profile</small></h1>
-</section>
-
-<!-- Main content -->
-<section class="content container-fluid">
-</section>
