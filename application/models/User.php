051284ad53929dccdd29919d9092d01d1d691371
agoenks29D <agungmasda29@gmail.com>
2021-10-06 01:14:47 +0700

    update models

--- application/models/User.php
+++ application/models/User.php
@@ -26,6 +26,16 @@ class User extends MY_Model
 		$this->db->where('password', sha1($password));
 		return $this->db->get($this->table);
 	}
+
+	public function search($identity = NULL) {
+		$this->db->group_start()
+			->where('username', $identity)
+			->or_group_start()
+				->where('email', $identity)
+			->group_end()
+		->group_end();
+		return $this->db->get($this->table);
+	}
 }
 
 /* End of file User.php */
