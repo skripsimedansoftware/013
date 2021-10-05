ca9e51dc5298483e26c6cb1ce2b357c506d1ca93
agoenks29D <agungmasda29@gmail.com>
2021-10-06 01:40:01 +0700

    update email confirm

--- application/models/Email_confirm.php
+++ application/models/Email_confirm.php
@@ -16,17 +16,12 @@ class Email_confirm extends MY_Model
 		$this->set_table('email_confirm');
 	}
 
-	public function new($user_uid = NULL, $confirm_code = '1337', $type = 'reset-password') {
-		$this->db->insert('email_confirm', array('type' => $type, 'user_uid' => $user_uid, 'confirm_code' => $confirm_code, 'expire_date' => nice_date(unix_to_human(strtotime('+ 1 day')), 'Y-m-d H:i:s')));
+	public function new_code($user_uid = NULL, $confirm_code = '1337', $type = 'reset-password') {
+		return $this->create(array('type' => $type, 'user_uid' => $user_uid, 'confirm_code' => $confirm_code, 'expire_date' => nice_date(unix_to_human(strtotime('+ 1 day')), 'Y-m-d H:i:s')));
 	}
 
-	public function detail($where) {
-		return $this->db->get_where('email_confirm', $where);
-	}
-
-	public function confirm($confirm_code)
-	{
-		return $this->db->update('email_confirm', array('status' => 'confirmed'), array('confirm_code' => $confirm_code));
+	public function confirm($confirm_code) {
+		return $this->update(array('status' => 'confirmed'), array('confirm_code' => $confirm_code));
 	}
 }
 
