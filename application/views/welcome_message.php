b68a3b435da7d9767d9f75266974749e2bda8a38
agoenks29D <agungmasda29@gmail.com>
2021-10-06 01:32:47 +0700

    add link to login

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -80,6 +80,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<code>application/controllers/Welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p><a href="<?php echo base_url('admin') ?>">Login Here</a></p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
