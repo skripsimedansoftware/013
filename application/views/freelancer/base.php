f44a71894a88792c6a6fa0c8b7907385db8c0c52
agoenks29D <agungmasda29@gmail.com>
2021-10-24 22:52:00 +0700

    update freelancer base

--- application/views/freelancer/base.php
+++ application/views/freelancer/base.php
@@ -187,7 +187,6 @@ desired effect
 				<!-- Optionally, you can add icons to the links -->
 				<li class="<?php echo $this->router->fetch_method() == 'index'?'active':'' ?>"><a href="<?php echo base_url($this->router->fetch_class()) ?>"><i class="fa fa-home"></i> <span>Home</span></a></li>
 				<li class="<?php echo (in_array($this->router->fetch_method(), ['project', 'saw_freelance']))?'active':'' ?>"><a href="<?php echo base_url($this->router->fetch_class().'/project') ?>"><i class="fa fa-map"></i> <span>Project</span></a></li>
-				<li class="<?php echo $this->router->fetch_method() == 'criteria'?'active':'' ?>"><a href="<?php echo base_url($this->router->fetch_class().'/criteria') ?>"><i class="fa fa-puzzle-piece"></i> <span>Criteria</span></a></li>
 			</ul>
 			<!-- /.sidebar-menu -->
 		</section>
