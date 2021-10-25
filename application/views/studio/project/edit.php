47949ddfd6c0eea1eb09081640a2af1995001736
Agung Dirgantara <agungmasda29@gmail.com>
2021-10-26 05:22:52 +0700

    update for currency

--- application/views/studio/project/edit.php
+++ application/views/studio/project/edit.php
@@ -30,12 +30,12 @@
 						</div>
 						<div class="form-group">
 							<label>Area</label>
-							<input type="text" class="form-control" name="area" placeholder="Project Area" value="<?php echo set_value('area', $project->area) ?>">
+							<input type="text" class="form-control" name="area" placeholder="Project Area" value="<?php echo number_format(set_value('area', $project->area), 2) ?>" data-type="meter">
 							<?php echo form_error('area', '<span class="help-block error">', '</span>'); ?>
 						</div>
 						<div class="form-group">
 							<label>Budget</label>
-							<input type="text" class="form-control" name="budget" placeholder="Project Budget" value="<?php echo set_value('budget', $project->budget) ?>">
+							<input type="text" class="form-control" name="budget" placeholder="Project Budget" value="<?php echo number_format(set_value('budget', $project->budget), 2) ?>" data-type="currency">
 							<?php echo form_error('budget', '<span class="help-block error">', '</span>'); ?>
 						</div>
 						<div class="form-group">
